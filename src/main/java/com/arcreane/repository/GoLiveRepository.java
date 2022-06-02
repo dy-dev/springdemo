28618e49b27c8c2b6e8e29ac07eb69dde1a8d4b2
dy-dev <dominique.yolin@free.fr>
2022-06-02 14:27:11 +0200

    Implementation of toString in Movie Entity and addMovie in GoLiveRepository (which uses a file to store information)

--- src/main/java/com/arcreane/repository/GoLiveRepository.java
+++ src/main/java/com/arcreane/repository/GoLiveRepository.java
@@ -7,6 +7,13 @@ import java.io.IOException;
 
 public class GoLiveRepository implements MovieRepositoryInterface {
     public void add(Movie p_Movie){
-
+        FileWriter fileWriter;
+        try{
+            fileWriter = new FileWriter("./tmp.txt", true);
+            fileWriter.write(p_Movie.toString() + "\n");
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
