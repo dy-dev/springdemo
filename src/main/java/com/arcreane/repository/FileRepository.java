b61b828e3eba064a9d5d1e26bac7758a7baf6faf
dy-dev <dominique.yolin@free.fr>
2022-06-02 17:08:34 +0200

    Property File in FileRepository automatically filled in applicationContext.xml Autowiring byName

--- src/main/java/com/arcreane/repository/FileRepository.java
+++ src/main/java/com/arcreane/repository/FileRepository.java
@@ -1,16 +1,27 @@
 package com.arcreane.repository;
 
 import com.arcreane.entity.Movie;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 @NoArgsConstructor
 public class FileRepository implements MovieRepositoryInterface {
+    private
+    @Getter
+    @Setter
+    File file;
+
     public void add(Movie p_Movie){
         FileWriter fileWriter;
         try{
-            fileWriter = new FileWriter("./tmp.txt", true);
+//            fileWriter = new FileWriter("./tmp.txt", true);
+            fileWriter = new FileWriter(file, true);
             fileWriter.write(p_Movie.toString() + "\n");
             fileWriter.close();
         } catch (IOException e) {
