e30840a942d9f4eb61f207609de05415d580d6b5
dy-dev <dominique.yolin@free.fr>
2022-06-09 14:37:06 +0200

    applicationContext.xml cleanup App.java annoted with configuration and componentscan, the use of xml file is limited only to give the application.property file path

--- src/main/java/com/arcreane/App.java
+++ src/main/java/com/arcreane/App.java
@@ -2,14 +2,20 @@ package com.arcreane;
 
 import com.arcreane.controller.MovieController;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
  */
+@Configuration
+@ComponentScan({"com.arcreane.controller","com.arcreane.service","com.arcreane.repository.file" })
+//@PropertySource("classpath:application.properties")
+@ImportResource("classpath:applicationContext.xml")
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         MovieController controller = (MovieController)context.getBean(MovieController.class);
         controller.addUsingConsole();
     }
